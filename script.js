7026caedc1377eefbba93d9d0826e0dd4ad8accf
MAJ <115960116+amzatmaji@users.noreply.github.com>
2022-10-22 02:13:50 +0100

    adding some interactivity

---
+++ script.js
@@ -0,0 +1,19 @@
+let header = document.getElementById('top');
+function firstFunc() {
+    header.style.backgroundColor = 'blue';
+}
+header.onclick = firstFunc;
+
+let tel = document.getElementsByClassName('tel')[0];
+let email = document.getElementsByClassName('email')[0];
+function secondFunc() {
+    tel.style.backgroundColor = 'orange';
+    tel.style.border = 'none'
+}
+function thirdFunc() {
+    email.style.backgroundColor = 'orangered';
+    email.style.border = 'none';
+}
+tel.onmouseover = secondFunc;
+email.onmouseover = thirdFunc;
+
